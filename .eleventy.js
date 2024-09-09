df4eebbba27bd1082b89a1ec12b49329f72d039b
maxniederer <66148024+maxniederer@users.noreply.github.com>
2024-09-09 12:12:00 -0400

    table of contents with anchored headings

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,30 @@
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+
+const pluginTOC = require("eleventy-plugin-toc");
+
 module.exports = function (eleventyConfig) {
+  // Markdown
+  eleventyConfig.setLibrary(
+    "md",
+    markdownIt({ html: true }).use(markdownItAnchor, {
+      permalink: markdownItAnchor.permalink.linkAfterHeader({
+        style: "visually-hidden",
+        assistiveText: (title) => `Permalink to “${title}”`,
+        visuallyHiddenClass: "visually-hidden",
+        space: false,
+        // placement: "after",
+        wrapper: ['<div class="heading-wrapper">', "</div>"],
+      }),
+    })
+  );
+
+  //table of contents
+  eleventyConfig.addPlugin(pluginTOC, {
+    tags: ["h2", "h3"],
+    wrapper: "div",
+  });
+
   // Copy `img/` to `_site/img`
   eleventyConfig.addPassthroughCopy("src/assets");
 
