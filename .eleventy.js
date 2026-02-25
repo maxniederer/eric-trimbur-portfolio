c6ae4e9afcd98a8f9758ad8e292ef43cffa46f67
maxniederer <66148024+maxniederer@users.noreply.github.com>
2026-02-25 18:52:30 -0500

    fix header anchors

--- .eleventy.js
+++ .eleventy.js
@@ -1,31 +1,52 @@
 const { DateTime } = require("luxon");
 
 const markdownIt = require("markdown-it");
+const markdownItAttrs = require("markdown-it-attrs");
 const markdownItAnchor = require("markdown-it-anchor");
+const markdownItDefList = require("markdown-it-deflist");
 
-// const pluginTOC = require("eleventy-plugin-toc");
 const pluginTOCN = require("eleventy-plugin-nesting-toc");
 const embedEverything = require("eleventy-plugin-embed-everything");
 
 module.exports = function (eleventyConfig) {
   // Markdown
-  eleventyConfig.setLibrary(
-    "md",
-    markdownIt({ html: true }).use(markdownItAnchor, {
-      permalink: markdownItAnchor.permalink.linkAfterHeader({
-        style: "visually-hidden",
-        assistiveText: (title) => `Permalink to “${title}”`,
-        visuallyHiddenClass: "visually-hidden",
-        space: false,
-        // placement: "after",
-        wrapper: ['<div class="heading-wrapper">', "</div>"],
-      }),
+  // eleventyConfig.setLibrary(
+  //   "md",
+  //   markdownIt({ html: true })
+  //     .use(markdownItAnchor, {
+  //     permalink: markdownItAnchor.permalink.linkAfterHeader({
+  //       style: "visually-hidden",
+  //       assistiveText: (title) => `Permalink to “${title}”`,
+  //       visuallyHiddenClass: "visually-hidden",
+  //       space: false,
+  //       // placement: "after",
+  //       wrapper: ['<div class="heading-wrapper">', "</div>"],
+  //     }),
+  //   })
+  // );
+
+  const markdownItOptions = {
+    html: true,
+    breaks: true,
+    linkify: true,
+  };
+  const markdownLib = markdownIt(markdownItOptions)
+    .use(markdownItAttrs, {
+      allowedAttributes: ['id', 'class', /^regex.*$/]
     })
-  );
+    .use(markdownItAnchor, {
+      permalink: markdownItAnchor.permalink.linkInsideHeader({
+        style: "aria-label",
+        assistiveText: (title) => `Permalink to “${title}”`,
+      })
+    })
+    .use(markdownItDefList);
+  eleventyConfig.setLibrary("md", markdownLib);
+
 
   //table of contents, nested
   eleventyConfig.addPlugin(pluginTOCN, {
-    tags: ["h2", "h3", "h4", "h5"],
+    tags: ["h2", "h3", "h4", "h5", "h6"],
     wrapper: "div",
   });
 
